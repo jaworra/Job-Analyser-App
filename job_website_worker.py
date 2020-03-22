1fed93d3a8ab08d7a4ccce37f93cd6d1546c1f25
jaworra <john.a.worrall@gmail.com>
2020-03-22 11:42:07 +1000

    new file - seperating webscraping methods

---
+++ job_website_worker.py
@@ -0,0 +1,48 @@
+#Web scapper worker on desktop
+#Todo: update for lambda or docker enviroment
+
+from selenium import webdriver
+import requests
+import webscrapping_worker as webscape
+
+def scaper_via_webdrivers(html_page):
+    '''
+    method uses selenium - webdrivers
+    requires webpage
+    '''
+    #https://www.edureka.co/blog/web-scraping-with-python/
+
+    #place chrome driver in folder and added to path
+    driver = webdriver.Chrome(executable_path=r'C:\webdrivers\chromedriver.exe') #edit this to just look in path variable
+    driver.get(page_link1)
+    print("Page Title is : %s" %driver.title)
+    driver.quit()
+
+    jobs=[] #List to job titles name
+    salaries=[] #List of job salaries  
+    ratings=[] #List to job rating
+    #driver.get("<a href="https://www.flipkart.com/laptops/">https://www.flipkart.com/laptops/</a>~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniq")
+    return
+
+def scaper_via_requests(html_page):    
+    '''
+    method with requests
+    requires webpage
+    '''
+    #https://codeburst.io/web-scraping-101-with-python-beautiful-soup-bb617be1f486
+
+    try:
+        page_response = requests.get(html_page, timeout=5)
+    except:
+        print("error - with calling website")
+
+    webscape.seek_isolate_contents(page_response,'seek')
+
+#scrape from the following websites
+page_link1 = 'https://www.seek.com.au/statistician-jobs'
+page_link2 = 'https://www.seek.com.au/mathematicians-jobs'
+
+#scaper_via_webdrivers depricated in favour of scaper_via_webdrivers due to transition to lambda code base libraries
+#scaper_via_webdrivers(page_link1) 
+scaper_via_requests(page_link1)
+
---
+++ webscrapping_worker.py
@@ -0,0 +1,78 @@
+#isolating contents from different webpages
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def seek_isolate_contents (page_response, company):
+    '''
+    Looks at Seek page to isolate relevant material to return
+
+
+    '''
+
+    print(company)
+    page_content = BeautifulSoup(page_response.content, "html.parser")
+
+    #just body extract with charcters
+    print(page_content.body.div.div.div.text.strip()) #Statistician Jobs in All Australia
+    
+    for hit in page_content.findAll(attrs={'class' : '_2iNL7wI'}):
+        job_title = hit.text.strip()
+        job_type = hit.find_next("p").text.strip().replace("This is a ","").replace("job","").rstrip()
+        print(job_title + " (" + job_type + ")") #returns all jobs   
+
+
+    #remove this below code...
+    for hit2 in page_content.findAll(attrs={'class' : ['Eadjc1o','_2iNL7wI']}):
+        job_descrip = hit2.text.strip() 
+        print(job_descrip)     
+    '''
+    body = page_content.find('body')
+    for job_title in page_content.findAll(attrs={'class' : '_2iNL7wI'}):
+        print(job_title) #returns all jobs  
+        #tag = job_title.find_next("p")
+        #print(tag)
+        #for job_descrip in job_title.findAll(attrs={'class' : 'Eadjc1o'}):
+        #    print(job_descrip) #returns all job
+        #hit = hit.text.strip()
+         
+    '''
+    #print(page_content.body.div.div.text.strip()) 
+    body_section = page_content.body.div.div
+    #print(page_content.body.div.div) 
+    #mydivs = page_content.findAll("body", class_= "_2iNL7wI")
+    #print(mydivs)
+    #soup = BeautifulSoup(html_page)
+    #print(page_content.find('div data-search-sol-meta='))
+    #print(page_content.find("body", id="aria-label"))
+    #extract body from seek html
+    #body = page_content.find('body')
+    #print(body)
+    #print(soup.find("body", class_="col-l-4 mtop pagination-number")["aria-label"] )
+    return
+
+    textContent = []
+    job_titles = []
+    for i in range(0, 20):
+        paragraphs = page_content.find_all("p")[i].text
+        textContent.append(paragraphs) 
+
+
+    return
+
+    '''
+    #Future implemenation
+    #loop through a list of job sites
+    dataList = [{'seek_stat': 'https://www.seek.com.au/statistician-jobs'},
+                {'seek_math': 'https://www.seek.com.au/mathematicians-jobs'},
+                {'indeed_stat': 'https://au.indeed.com/jobs?q=statistician&l='},
+                {'indeed_math': 'https://au.indeed.com/jobs?q=mathematician&l='},
+                {'jora_stat': 'https://au.jora.com/j?q=Statistician&l=Australia&button=&sp=search'}, # format -- https://au.jora.com/cms/get-your-feed-included-on-jora
+                {'jora_math': 'https://au.jora.com/j?q=Mathematician&l=Australia&button=&sp=search#email_alert_modal'},
+                {'linkedIn_math': 'https://www.linkedin.com/jobs/search/?geoId=101452733&keywords=statistician&location=Australia'},
+                {'linkedIn_math': 'https://www.linkedin.com/jobs/search/?keywords=mathematician'},
+                #{'monster_math': ''}, #not provided in australia
+                ]
+    for index in range(len(dataList)):
+        for key in dataList[index]:
+            print(dataList[index][key])
+    '''
